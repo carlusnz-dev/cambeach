1e25d6a4d6c134f5de22f321f5dd6962d4eaa767
Carlos Antunes <carlosantunes.dev@gmail.com>
2025-11-11 13:22:57 -0300

    update all styles, add style.css

--- cambeach/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.lista_tarefas, name='lista_tarefas'),
-    path('nova', views.adicionar_tarefa, name='nova_tarefa')
-]
