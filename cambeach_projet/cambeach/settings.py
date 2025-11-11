fa5682f7bb065ffc8d8ad007f8c640597b1225c9
Carlos Antunes <carlosantunes.dev@gmail.com>
2025-11-11 15:05:39 -0300

    config complete django

--- cambeach_projet/cambeach/settings.py
+++ cambeach_projet/cambeach/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cambeach_app' , 
     'users' , 
-    
 ]
 
 MIDDLEWARE = [
@@ -117,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/cambeach_app/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- cambeach_projet/users/models.py
+++ cambeach_projet/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  email = models.EmailField(unique=True)
+  password = models.CharField(max_length=255)
+  name = models.CharField(max_length=255)
+  cpf = models.CharField(max_length=14)
+  telephone = models.CharField(max_length=14)
+  role = models.SmallIntegerField()
+  age = models.SmallIntegerField()
+  created_at = models.DateTimeField(auto_now_add=True)
