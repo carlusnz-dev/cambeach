47d7fdc62ce2f357bf08a1d89865068e98667424
Carlos Antunes <carlosantunes.dev@gmail.com>
2025-11-14 17:53:45 -0300

    category CRUD completo

--- CAMB_proj/cambeach_app/urls.py
+++ CAMB_proj/cambeach_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio),
+    path('', views.inicio, name='home'),
+    
+    # Categorias
+    path('create/', views.category_form, name='category_create'),
+    path('update/<int:pk>/', views.category_form, name='category_update'),
+    path('delete/<int:pk>/', views.category_delete, name='category_delete'),
 ]
--- CAMB_proj/cambeach_app/views.py
+++ CAMB_proj/cambeach_app/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Category, Gender
+from .forms import CategoryForm
 
 def inicio(request):
-    return render(request, 'inicio.html')
+    categories = Category.objects.all()
+    
+    context = {
+        'categories': categories
+    }
+    
+    return render(request, 'inicio.html', context)
+
+# Categorias
+def category_form(request, pk=None):
+    category = None
+    
+    if pk:
+        category = get_object_or_404(Category, pk=pk)
+    
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    else:
+        form = CategoryForm(instance=category)
+        
+    context = {'form': form, 'category': category}
+    return render(request, 'category_form.html', context)
+
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk)
+    
+    category.delete()
+    return redirect('home')
