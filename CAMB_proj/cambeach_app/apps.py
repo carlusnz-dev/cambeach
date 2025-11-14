13d62bfa2abd10f3a76313c3459141dc9995cabc
Carlos Antunes <carlosantunes.dev@gmail.com>
2025-11-14 15:14:40 -0300

    add .gitignore

--- CAMB_proj/cambeach_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CambeachAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cambeach_app'
--- README.md
+++
@@ -1,6 +0,0 @@
-# CamBeach
-Gerenciamento de campeonato de Beach Tênis
-
-## Estrutura
-- cambeach = BACKEND DJANGO
-- cambeach-front = FRONTEND
